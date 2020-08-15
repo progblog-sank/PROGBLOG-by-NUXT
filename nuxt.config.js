d54005a5c88b5399de37a0ceeb3f0779d92a9610
progblog <info@progblog-web.com>
2020-08-15 19:50:58 +0900

    DESC

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'シングルページアプリケーションを実装できるnuxt.jsで作成したPROGBLOGのポートフォリオサイト' },
+      { hid: 'description', name: 'description', content: 'NUXT + Netlify + エックスサーバー + microCMS で作成したPROGBLOGのポートフォリオサイト' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'PROGBLO by NUXT' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: 'https://nuxt.progblog-web.com' },
