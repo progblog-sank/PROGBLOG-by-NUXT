a49a1c5bb701109b37dfedf816e64e8c88b42ba7
progblog <info@progblog-web.com>
2020-08-16 01:43:55 +0900

    pwaの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,5 +109,24 @@ export default {
         icons: ['fas']
       }
     ]
+  },
+    /*
+   ** PWAの設定
+   */
+  manifest: {
+    name: 'PROGBLO by NUXT',      // アプリの名称
+    short_name: 'PROGBLO',    // ホーム画面に表示される名称
+    display: 'standalone',     // 表示モード  'fullscreen' 'standalone' 'minimal-ui' 'browser'
+    theme_color: '#ffb8ab',    // アプリケーションの既定のテーマ色を定義
+    background_color: '#ffffff',  // 背景の色
+    lang: 'ja',                // 言語
+    start_url: '/?mode=pwa',   // アイコンから起動した時のURL
+    icons: [
+      {
+        src: '/icon.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ],
   }
 }
